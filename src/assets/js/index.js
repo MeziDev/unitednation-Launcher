66038fc0b513322b76fdf67c75104a08ba4b66bc
MeziDev <meziofficielleyt@gmail.com>
2024-06-04 03:32:52 +0200

    publish 1.0.0

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "Mezi" },
+            { "message": "Salut je suis du code.", "author": "Mezi" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Mezi" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
